f815ce81a8cee4a98c7b20157b3e030faf0009e7
Jerry Chai <zic337@utulsa.edu>
2018-02-14 23:18:46 +0000

    User friendly stdout

--- install.sh
+++ install.sh
@@ -8,19 +8,15 @@ olddir=~/dotfiles_old/$(date +%F_%R)             # old dotfiles backup directory
 file_path_abs=$(find $dir/dotfiles -type f)    # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
 if [ -d $olddir ]; then
+    echo "Creating $olddir for backup."
     mkdir -p $olddir
 fi
 
-
 for file in $file_path_abs; do
     file_path_relative=$(echo $file | sed s#$dir/dotfiles/##)
-    echo $file_path_relative
     file_home=~/$file_path_relative  # file expected in ~
-    echo $file_home
     file_backup="$olddir/$file_path_relative"  # backup file
-    echo $file_backup
 
     # backup files along with its directory in ~ to $olddir
     if [ -e "$file_home" ]; then
@@ -29,12 +25,12 @@ for file in $file_path_abs; do
             mkdir -p $file_backup
             rm -r $file_backup
         fi
-        echo "Moving $file_home to $file_backup"
+        echo "Moving $file_home to $file_backup."
         mv $file_home $file_backup  # we intended to use mv not cp here
     fi
 
     # creating symlink to $file in home directory
-    echo "Creating symlink to $file in home directory."
+    echo "Creating symlink from $file to $file."
     # Create directory before creating sysmlink if necessary
     if [ ! -e $file_home ]; then
         mkdir -p $file_home
